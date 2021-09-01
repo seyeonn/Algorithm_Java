218ab4e7d2c050e10ec821ad7f4ddddf5a0c58d7
seyeonn <ansse17@gmail.com>
2021-09-02 01:43:07 +0900

    20210902

---
+++ src/com/practice/Baekjoon/Math1/BJ2747.java
@@ -0,0 +1,21 @@
+package com.practice.Baekjoon.Math1;
+
+import java.util.Scanner;
+
+public class BJ2747 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int N = scan.nextInt();
+        int[] fibo = new int[N*10];
+
+        fibo[0] = 0;
+        fibo[1] = 1;
+
+        for (int i = 2; i <= N; i++) {
+            fibo[i] = fibo[i-1] + fibo[i-2];
+        }
+        System.out.println(fibo[N]);
+    }
+}
