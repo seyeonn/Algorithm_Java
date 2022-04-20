74b90102682d24fd5f85a708dfd24de4b6b388d0
seyeonn <ansse17@gmail.com>
2022-04-21 00:53:23 +0900

    220421

---
+++ src/com/practice/Baekjoon/Realization/BJ14910.java
@@ -0,0 +1,37 @@
+package beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class BJ14910 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int temp = Integer.MIN_VALUE;
+		String ans = "Good";
+		
+		while(st.hasMoreTokens()) {
+			
+			int N = Integer.parseInt(st.nextToken());
+			
+			if(temp <= N) {
+				temp = N;
+				continue;
+			}else {
+				ans = "Bad";
+				break;
+			}
+		}
+		
+		System.out.println(ans);
+	}
+
+}
