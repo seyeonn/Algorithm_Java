0a698f390d1701a76c8552eaf93c7621262ebf29
seyeonn <ansse17@gmail.com>
2022-04-25 22:13:32 +0900

    220425

---
+++ src/com/practice/Programmers/PG_K번째수.java
@@ -0,0 +1,20 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class PG_K¹øÂ°¼ö {
+
+	public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        for(int i=0; i<commands.length; i++) {
+            int[] temp = new int[commands[i][1] - commands[i][0] + 1];
+            for(int j=0; j<temp.length; j++) {
+                temp[j] = array[(commands[i][0] -1) +j];
+            }
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2] -1];
+        }
+        return answer;
+    }
+}
