55cb79e286f0867055aa3551228a2e417e27e683
seyeonn <ansse17@gmail.com>
2022-04-28 00:59:37 +0900

    220428

---
+++ src/com/practice/Programmers/PG_완주하지못한선수.java
@@ -0,0 +1,18 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class PG_완주하지못한선수 {
+
+	public String solution(String[] participant, String[] completion) {
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        int i;
+        
+        for(i=0; i<completion.length; i++) {
+                if(!participant[i].equals(completion[i]))
+                    return participant[i];
+        }
+        return participant[i];
+    }
+}
