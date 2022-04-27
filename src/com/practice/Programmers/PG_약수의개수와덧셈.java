6d4d7de0dfd5a8b2fb6f7831a6948e41449aa06f
seyeonn <ansse17@gmail.com>
2022-04-28 01:19:44 +0900

    220428

---
+++ src/com/practice/Programmers/PG_약수의개수와덧셈.java
@@ -0,0 +1,27 @@
+package programmers;
+
+public class PG_약수의개수와덧셈 {
+	
+	public int solution(int left, int right) {
+        int answer = 0;
+        int[] arr = new int[right-left+1];
+        int odd = 0;
+        
+        for(int i=0; i<arr.length; i++) {
+            arr[i] = left++;
+            
+            for(int j=1; j<33; j++) {
+                if(j*j == arr[i]) {
+                    odd = arr[i];
+                }
+            }
+            
+            if(arr[i] == odd)
+                answer -= arr[i];
+            else
+                answer += arr[i];
+        }
+        
+        return answer;
+    }
+}
