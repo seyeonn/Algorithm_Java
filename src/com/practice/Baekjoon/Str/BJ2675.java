a9710e05a36de2e4266ed28285d48b64e432ab53
seyeonn <ansse17@gmail.com>
2022-05-26 01:27:38 +0900

    220526

---
+++ src/com/practice/Baekjoon/Str/BJ2675.java
@@ -0,0 +1,29 @@
+package com.practice.Baekjoon.Str;
+
+import java.util.Scanner;
+
+public class BJ2675 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+
+		int t = scan.nextInt();
+
+		String temp = "";
+		
+		for(int i=0; i<t; i++) {
+			int r = scan.nextInt();	
+			String s = scan.next();
+
+			for(int j=0; j<s.length(); j++) {
+				for(int k=0; k<r; k++) {
+					temp += s.charAt(j);
+				}
+			}
+			temp += "\n";
+		}
+		System.out.println(temp);
+	}
+
+}
