deb857d7cac3e2c81be683484a7c450ce2b25d42
seyeonn <ansse17@gmail.com>
2022-07-05 17:49:16 +0900

    220705

---
+++ src/com/practice/Programmers/PG_소수만들기.java
@@ -0,0 +1,35 @@
+package com.practice.Programmers;
+
+class PG_소수만들기 {
+    public int solution(int[] nums) {
+        
+        int sum = 0;
+        int answer = 0;
+        
+       for(int i=0; i<nums.length; i++){
+           for(int j=i+1; j<nums.length; j++){
+               for(int k=j+1; k<nums.length; k++) {
+                       if(calc(nums[i] + nums[j] + nums[k])) {
+                           answer++;
+                       }
+                   
+               }
+           }
+       }
+        return answer;
+    }
+    
+    public boolean calc(int sum) {
+        int count = 0;
+        for(int i=1; i<=sum; i++) {
+            if(sum%i == 0) {
+                count++;
+            }
+        }
+        
+        if(count == 2)
+            return true;
+        else
+            return false;
+    }
+}
