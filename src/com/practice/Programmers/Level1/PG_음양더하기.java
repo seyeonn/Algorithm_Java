89ca54380c89d915086132936c6bf3a2f978da98
seyeonn <ansse17@gmail.com>
2022-07-08 23:14:56 +0900

    220708

---
+++ src/com/practice/Programmers/Level1/PG_음양더하기.java
@@ -0,0 +1,16 @@
+package com.practice.Programmers.Level1;
+
+class PG_음양더하기 {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        
+        for(int i=0; i<absolutes.length; i++) {
+            if(signs[i]) {
+                answer += absolutes[i];
+            }else {
+                answer -= absolutes[i];
+            }
+        }
+        return answer;
+    }
+}
