dde8015ce71f77c14b897cdf5c3e95895571bc3a
seyeonn <ansse17@gmail.com>
2022-07-08 23:04:46 +0900

    220708

---
+++ src/com/practice/Programmers/PG_내적.java
@@ -0,0 +1,12 @@
+package com.practice.Programmers;
+
+class PG_내적 {
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+        
+        for(int i=0; i<a.length; i++) {
+            answer += a[i] * b[i];
+        }
+        return answer;
+    }
+}
