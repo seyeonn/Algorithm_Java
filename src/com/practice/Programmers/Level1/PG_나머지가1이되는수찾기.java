474b6def747eab18d3cbe43a9f2cddb51410f644
seyeonn <ansse17@gmail.com>
2022-07-11 23:52:32 +0900

    220711

---
+++ src/com/practice/Programmers/Level1/PG_나머지가1이되는수찾기.java
@@ -0,0 +1,15 @@
+package com.practice.Programmers.Level1;
+
+class PG_나머지가1이되는수찾기 {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(int i=1; i<n; i++) {
+            if(n%i == 1){
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+}
