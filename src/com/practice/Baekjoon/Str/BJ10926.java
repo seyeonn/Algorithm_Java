2f1b3fecddc38473cbd641e9800edeb15501bbb2
seyeonn <ansse17@gmail.com>
2022-07-14 01:45:48 +0900

    220714

---
+++ src/com/practice/Baekjoon/Str/BJ10926.java
@@ -0,0 +1,12 @@
+package com.practice.Baekjoon.Str;
+
+import java.util.Scanner;
+
+public class BJ10926 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println(scan.next() + "??!");
+
+    }
+}
---
+++ src/com/practice/Baekjoon/Str/BJ18108.java
@@ -0,0 +1,11 @@
+package com.practice.Baekjoon.Str;
+
+import java.util.Scanner;
+
+public class BJ18108 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println(scan.nextInt()-543);
+    }
+}
