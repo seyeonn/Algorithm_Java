6c67ea2021b0aea17669b9c411a3187544534d38
seyeonn <ansse17@gmail.com>
2022-09-12 01:56:23 +0900

    220912

---
+++ src/com/practice/Baekjoon/Math1/BJ10998.java
@@ -0,0 +1,16 @@
+package com.practice.Baekjoon.Math1;
+
+import java.util.Scanner;
+
+public class BJ10998 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		
+		System.out.println(a*b);
+	}
+}
